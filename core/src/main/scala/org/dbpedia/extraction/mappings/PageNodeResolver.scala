b1a31c8545d7a80a49ffb3d69bc2869d656b7822
fernandez <fernandez@ai.wu.ac.at>
2016-09-28 18:53:32 +0200

    major revision code

--- core/src/main/scala/org/dbpedia/extraction/mappings/PageNodeResolver.scala
+++
@@ -1,5 +0,0 @@
-package org.dbpedia.extraction.mappings
-
-import org.dbpedia.extraction.wikiparser.PageNode
-
-trait PageNodeResolver extends Resolver[PageNode]
--- core/src/main/scala/org/dbpedia/extraction/mappings/PropertyResolver.scala
+++
@@ -1,5 +0,0 @@
-package org.dbpedia.extraction.mappings
-
-import org.dbpedia.extraction.wikiparser.TemplateNode
-
-trait PropertyResolver extends Resolver[TemplateNode]

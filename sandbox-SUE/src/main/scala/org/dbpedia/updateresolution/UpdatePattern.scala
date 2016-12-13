c2ed930b0a91dc3f72249cc3ca39894d1df2d663
Vadim Savenkov <vadim.savenkov@wu.ac.at>
2016-12-13 12:29:28 +0100

    UpdatePattern

--- sandbox-SUE/src/main/scala/org/dbpedia/updateresolution/UpdatePattern.scala
+++ sandbox-SUE/src/main/scala/org/dbpedia/updateresolution/UpdatePattern.scala
@@ -2,15 +2,18 @@ package org.dbpedia.updateresolution
 
 import org.dbpedia.extraction.destinations.Quad
 
+import scala.collection.mutable.Buffer
+import scala.collection.mutable.ArrayBuffer
+
 class UpdatePattern (
-    val infoboxType: String, // should be URI, what is a relevant datatype
-    val wikiDelete: Seq[Quad] = Seq(), // each triple represents a deleted Wiki property
-    val wikiInsert: Seq[Quad] = Seq(), // each triple represents an inserted Wiki property using variables
-    val rdfDelete: Seq[Quad] = Seq(),  // deleted dbpedia triples
-    val rdfInsert: Seq[Quad] = Seq(), // inserted dbpedia triples using variable
-    val origin: String = UpdatePattern.RDF_INSERT,
-    val inducedReason: InducedUpdateReason=null,
-    val inducedBy: UpdatePattern=null)
+        val infoboxType: String, // should be URI, what is a relevant datatype
+        val wikiDelete: Buffer[Quad] = ArrayBuffer[Quad](), // each triple represents a deleted Wiki property
+        val wikiInsert: Buffer[Quad] = ArrayBuffer[Quad](), // each triple represents an inserted Wiki property using variables
+        val rdfDelete: Buffer[Quad] = ArrayBuffer[Quad](), // deleted dbpedia triples
+        val rdfInsert: Buffer[Quad] = ArrayBuffer[Quad](), // inserted dbpedia triples using variable
+        val origin: String = UpdatePattern.RDF_INSERT,
+        val inducedReason: InducedUpdateReason=null,
+        val inducedBy: UpdatePattern=null)
   {
   override def toString(): String = toString(x=>x)
 
@@ -22,8 +25,12 @@ class UpdatePattern (
       case UpdatePattern.RDF_DELETE => (rdfDelete,wikiDelete,UpdatePattern.WIKI_DELETE)
       case UpdatePattern.WIKI_DELETE => (wikiDelete,rdfDelete,UpdatePattern.RDF_DELETE)
     }
-    if( to.isEmpty )
-      from.foldLeft(to)( (z, quad)=>z :+ quad.copy(predicate=propMap(quad.predicate),context=newctx) )
+
+    to.appendAll(
+      if( to.isEmpty )
+        from.foldLeft(Seq.empty[Quad])((qs, quad)=>qs :+ quad.copy(predicate=propMap(quad.predicate),context=newctx) )
+      else Seq.empty[Quad]
+    )
   }
 
   def toString( f: String => String ): String = {
@@ -55,7 +62,7 @@ object UpdatePattern {
   val STRING = "xsd:string"
   val PAGE = "?PAGE"
 
-  var lastVarName = "?A"
+  var lastVarName = "?@" // init with a char preceding A
 
   var WIKISIDE_UPDAE = "wiki"
   var RDFSIDE_UPDATE = "rdf"
@@ -68,46 +75,46 @@ object UpdatePattern {
 
   def wd(property:String ) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, freshVar, WIKI_DELETE, STRING)
-    new UpdatePattern(PAGE, wikiDelete=Seq(q))
+    new UpdatePattern(PAGE, wikiDelete=ArrayBuffer(q))
   }
   def wd(property:String, value:String ) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, value, WIKI_DELETE, STRING)
-    new UpdatePattern(PAGE, wikiDelete=Seq(q))
+    new UpdatePattern(PAGE, wikiDelete=ArrayBuffer(q))
   }
 
   def rd(property:String) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, freshVar, RDF_DELETE, STRING)
-    new UpdatePattern(PAGE, rdfDelete=Seq(q))
+    new UpdatePattern(PAGE, rdfDelete=ArrayBuffer(q))
   }
   def rd(property:String, objct: String) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, objct, RDF_DELETE, STRING)
-    new UpdatePattern(PAGE, rdfDelete = Seq(q))
+    new UpdatePattern(PAGE, rdfDelete = ArrayBuffer(q))
   }
 
   def wi(property:String ) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, freshVar, WIKI_INSERT, STRING)
-    new UpdatePattern(PAGE, wikiInsert=Seq(q))
+    new UpdatePattern(PAGE, wikiInsert=ArrayBuffer(q))
   }
   def wi(property:String, value:String ) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, value, WIKI_INSERT, STRING)
-    new UpdatePattern(PAGE, wikiInsert=Seq(q))
+    new UpdatePattern(PAGE, wikiInsert=ArrayBuffer(q))
   }
 
   def ri(property:String ) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, freshVar, RDF_INSERT, STRING)
-    new UpdatePattern(PAGE, rdfInsert=Seq(q))
+    new UpdatePattern(PAGE, rdfInsert=ArrayBuffer(q))
   }
   def ri(property:String, objct:String ) : UpdatePattern = {
     val q = new Quad(LANGUAGE, null, PAGE, property, objct, RDF_INSERT, STRING)
-    new UpdatePattern(PAGE, rdfInsert=Seq(q))
+    new UpdatePattern(PAGE, rdfInsert=ArrayBuffer(q))
   }
 
   def toRdf( wikiQuad:Quad, rdfProperty:String ) = wikiQuad.copy(context = rdfContext(wikiQuad.context), predicate = rdfProperty)
   def toWiki( rdfQuad:Quad, wikiProperty:String ) = rdfQuad.copy(context = wikiContext(rdfQuad.context), predicate = wikiProperty)
 
   def freshVar():String = {
-    val l = (lastVarName tail+1).toChar.toString
-    lastVarName = (if(l >= "Z") lastVarName + "A" else lastVarName dropRight(1)) + l
+    val l = (lastVarName.last + 1).toChar.toString
+    lastVarName = (if(l >= "Z") lastVarName + "A" else lastVarName.dropRight(1)) + l
     lastVarName
   }
   def rdfContext(context : String):String = {

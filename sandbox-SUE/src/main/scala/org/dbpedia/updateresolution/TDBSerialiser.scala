e19d6e5aa4661106a2fdbf86fc124855b053950d
Albin Ahmeti <albin.ahmeti@gmail.com>
2016-12-14 12:51:45 +0100

    TDBSerialiser class

--- sandbox-SUE/src/main/scala/org/dbpedia/updateresolution/TDBSerialiser.scala
+++ sandbox-SUE/src/main/scala/org/dbpedia/updateresolution/TDBSerialiser.scala
@@ -8,20 +8,26 @@ import com.hp.hpl.jena.query.ReadWrite
 import com.hp.hpl.jena.tdb.TDBFactory
 import org.dbpedia.extraction.destinations.Quad
 import org.dbpedia.extraction.destinations.formatters.TerseFormatter
+import org.dbpedia.extraction.ontology.OntologyProperty
+import org.dbpedia.extraction.util.Language
+import org.openrdf.query.Dataset
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Created by aahmeti on 13/12/2016.
  */
-class TDBSerialiser {
-
-  val download_directory = "./data/downloads"
+class TDBSerialiser
+(val download_directory:String="./data/downloads")
+{
   val dataset = TDBFactory.createDataset(download_directory + "/" + "TDB")
+  var ts:TripleStore = null
 
   def store(quads: Seq[Quad]): Unit = {
 
     dataset.begin(ReadWrite.WRITE)
     val model = dataset.getDefaultModel()
-    val ts = new TripleStore(model)
+    ts = new TripleStore(model)
     ts.setDataset(dataset)
 
     val t = new TerseFormatter(false, true)
@@ -33,7 +39,32 @@ class TDBSerialiser {
     }
 
     model.read( new ByteArrayInputStream(sb.toString().getBytes(StandardCharsets.UTF_8)), null, "N3" )
+  }
+
+  def query(query:String): Unit = {
+
+    println(ts.runQuery(query))
 
   }
 
 }
+
+object TestTDBSerialiser {
+  def main(args: Array[String]): Unit = {
+
+    val test = new TDBSerialiser()
+
+    val quads = new ArrayBuffer[Quad]()
+
+    val homepageProperty = new OntologyProperty("Foaf:homepage", Map(Language.English -> "homepage"), Map(), null, null, false, Set(), Set())
+    val quad1 = new Quad(null, null, "TestPage", homepageProperty , "http://example.com", null, null)
+
+    quads += quad1
+
+    test.store(quads)
+
+    test.query("SELECT * WHERE {}")
+  }
+}
+
+
